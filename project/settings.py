47ec21bde0016a8a0f4edf491f2c64111102268e
Tom <tom.stout325@gmail.com>
2025-04-21 00:52:17 -0400

    updated config

--- project/settings.py
+++ project/settings.py
@@ -39,12 +39,12 @@ INSTALLED_APPS = [
 
 # Middleware
 MIDDLEWARE = [
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
