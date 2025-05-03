e21442f389201f08110153997a1f4fb2cb0a43f1
Tom <tom.stout325@gmail.com>
2025-05-03 15:08:11 -0400

    updated home

--- project/urls.py
+++ project/urls.py
@@ -2,14 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import render
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
-    path('', include('finance.urls')),
-    path('', include('drones.urls')),
+    path('', include('app.urls')),    
+    path('finance/', include('finance.urls')),
+    path('drones/', include('drones.urls')),
 ]
+
 if not settings.USE_S3:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
