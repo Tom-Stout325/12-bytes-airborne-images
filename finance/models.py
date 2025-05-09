42001a3671ca3f7d5a7e2f44a653b22d87042aec
Tom <tom.stout325@gmail.com>
2025-05-08 23:00:24 -0400

    updated models

--- finance/models.py
+++ finance/models.py
@@ -95,7 +95,7 @@ class Transaction(models.Model):
     team           = models.ForeignKey('Team', null=True, on_delete=models.PROTECT)
     transaction    = models.CharField(max_length=500, blank=True, null=True)
     tax            = models.CharField(max_length=500, blank=True, null=True, default="Yes")
-    keyword        = models.ForeignKey(Keyword, on_delete=models.PROTECT, default=1)
+    keyword = models.ForeignKey(Keyword, on_delete=models.PROTECT, null=True, blank=True)
     receipt        = models.FileField(upload_to='receipts/', blank=True, null=True)
     user           = models.ForeignKey(User, on_delete=models.PROTECT)
 
