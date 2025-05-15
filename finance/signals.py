7fefbea259f86316908c3b0a129f73393e9ea7cf
Tom <tom.stout325@gmail.com>
2025-05-15 17:30:45 -0400

    updating invoice form

--- finance/signals.py
+++ finance/signals.py
@@ -1,10 +1,22 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery
+from django.db.models import Value
 from .models import Invoice
 
 @receiver(post_save, sender=Invoice)
 def update_search_vector(sender, instance, **kwargs):
-    Invoice.objects.filter(pk=instance.pk).update(
-        search_vector=SearchVector('invoice_numb', 'client__business', 'service__service')
+    # Safely get joined values
+    client_business = instance.client.business if instance.client else ""
+    service_name = instance.service.service if instance.service else ""
+
+    # Combine values into one vector
+    vector = (
+        SearchVector(Value(instance.invoice_numb), weight='A') +
+        SearchVector(Value(client_business), weight='B') +
+        SearchVector(Value(service_name), weight='B')
     )
+
+    # Assign and save only the search_vector field
+    instance.search_vector = vector
+    instance.save(update_fields=['search_vector'])
