b7bded47bf29247848cd74a03f93455624cbb750
Tom <tom.stout325@gmail.com>
2025-05-15 17:37:43 -0400

    updating invoice form

--- finance/signals.py
+++ finance/signals.py
@@ -1,22 +1,27 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.postgres.search import SearchVector, SearchQuery
+from django.contrib.postgres.search import SearchVector
 from django.db.models import Value
 from .models import Invoice
 
 @receiver(post_save, sender=Invoice)
 def update_search_vector(sender, instance, **kwargs):
-    # Safely get joined values
-    client_business = instance.client.business if instance.client else ""
-    service_name = instance.service.service if instance.service else ""
+    from django.db.models.signals import post_save
 
-    # Combine values into one vector
-    vector = (
-        SearchVector(Value(instance.invoice_numb), weight='A') +
-        SearchVector(Value(client_business), weight='B') +
-        SearchVector(Value(service_name), weight='B')
-    )
+    # Disconnect to avoid infinite loop
+    post_save.disconnect(update_search_vector, sender=Invoice)
 
-    # Assign and save only the search_vector field
-    instance.search_vector = vector
-    instance.save(update_fields=['search_vector'])
+    try:
+        client_business = instance.client.business if instance.client else ""
+        service_name = instance.service.service if instance.service else ""
+
+        instance.search_vector = (
+            SearchVector(Value(instance.invoice_numb), weight='A') +
+            SearchVector(Value(client_business), weight='B') +
+            SearchVector(Value(service_name), weight='B')
+        )
+        instance.save(update_fields=['search_vector'])
+
+    finally:
+        # Reconnect after save
+        post_save.connect(update_search_vector, sender=Invoice)
